82b600b9d8217dfc759b3ed544e3054bbd77bea0
Tiago Bazzi Zancanella <tiagozancanella17@gmail.com>
2025-06-18 16:30:22 -0300

    backup do instaldor e modificado css do menu

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { providePrimeNG } from 'primeng/config';
 
 
 
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+// providePrimeNG({
+//   theme: {
+//     preset: Aura,
+//     options: {
+//       darkModeSelector: '.rmt-park',
+//     }
+//  }) }
