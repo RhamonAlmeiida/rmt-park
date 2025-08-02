b56c07b5ad3ebaa9d51ef477f97afc35bdf9cf91
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-08-02 12:37:11 -0300

    Adicionado campo blog na landing page

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -8,6 +8,7 @@ import { ToastModule } from 'primeng/toast';
 import { LoginService } from '../../services/login.service';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -21,6 +22,7 @@ import { CardModule } from 'primeng/card';
     DialogModule,
     ButtonModule,
     CardModule,
+    CommonModule,
     
   ],
   providers: [ConfirmationService, MessageService],
@@ -28,6 +30,30 @@ import { CardModule } from 'primeng/card';
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent implements OnInit {
+
+    posts = [
+  {
+    imagem: 'assets/blog1.jpg',
+    tempoLeitura: '6 min leitura',
+    data: '6 maio 2025',
+    titulo: 'Como evitar perdas com a cobrança manual de estacionamentos',
+    resumo: 'A cobrança em estacionamentos pode gerar erros e prejuízos. Saiba como minimizar perdas com soluções mais...'
+  },
+  {
+    imagem: 'assets/blog2.jpg',
+    tempoLeitura: '6 min leitura',
+    data: '6 maio 2025',
+    titulo: 'Estacionamento 24h: como manter a operação segura',
+    resumo: 'Um estacionamento 24 horas exige protocolos rigorosos de segurança. Entenda como proteger a operação, os clientes...'
+  },
+  {
+    imagem: 'assets/blog3.jpg',
+    tempoLeitura: '5 min leitura',
+    data: '6 maio 2025',
+    titulo: 'Sazonalidade em estacionamento: como se preparar para picos e quedas',
+    resumo: 'A sazonalidade no estacionamento afeta o volume de clientes. Veja como prever os ciclos e manter a lucratividade...'
+  }
+];
   ngOnInit() {
     console.log('LandingPage carregada');
   }
@@ -42,6 +68,8 @@ export class LandingPageComponent implements OnInit {
     this.router.navigate(['/login'])
     
   }
+
+
 }
 
 
